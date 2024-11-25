9c4dba2bff25c427412265fedc887a42186e64e7
alyanaduncan04 <alyanaduncan1@gmail.com>
2024-11-24 22:24:12 -0500

    Added "Edit-User" Functionality

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,6 +1,7 @@
 import { AddUser } from "./addUser.js";
+import { UserMembership } from "./userMembership.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    AddUser();
-  });
-  
+  AddUser();
+  UserMembership();
+});
